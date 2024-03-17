2b7cc0bee417304de1dc071db5a8b08b99b4c34a
Trần Hồng Quân <thquan2k3@gmail.com>
2024-03-17 11:21:08 +0700

    add simple dictionary

---
+++ Dictionary/src/dictionary/Dictionary.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dictionary;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ * @author Quan
+ */
+public class Dictionary {
+
+    private List<Word> engToViet = new ArrayList<Word>();
+    private List<Word> vietToEng = new ArrayList<Word>();
+
+    public Dictionary() {
+
+    }
+
+    private Connection connect() {
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            //File temp = new File("dict_hh.db");
+            //String url = "jdbc:sqlite:/" + temp.getAbsolutePath().replace("\\","\\\\");
+            String url = "jdbc:sqlite:dict_hh.db";
+            System.out.println(url);
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    private boolean readDatabase() {
+
+        String selectEngData = "SELECT * FROM av";
+        String selectVietData = "SELECT * FROM va";
+
+        try {
+            Connection conn = this.connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(selectEngData);
+
+            // loop through the result set  
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String word = rs.getString("word");
+                String pronounce = rs.getString("pronounce");
+                String description = rs.getString("description");
+                String html = rs.getString("html");
+                engToViet.add(new Word(id, word, html, pronounce, description));
+            }
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(selectVietData);
+
+            // loop through the result set  
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String word = rs.getString("word");
+                String pronounce = rs.getString("pronounce");
+                String description = rs.getString("description");
+                String html = rs.getString("html");
+                vietToEng.add(new Word(id, word, html, pronounce, description));
+            }
+
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public Word[] findAllEngWordContain(String find) {
+        List<Word> words = new ArrayList<Word>();
+
+        for (Word w : engToViet) {
+            if (w.getWord().startsWith(find)) {
+                words.add(w);
+            }
+        }
+
+        Collections.sort(words, new Comparator<Word>() {
+            @Override
+            public int compare(Word s1, Word s2) {
+                return s1.getWord().compareTo(s2.getWord());
+            }
+        });
+
+        return words.toArray(new Word[0]);
+    }
+    
+    public Word findEngWord(String find){
+        for(Word w : engToViet){
+            if(w.getWord().equals(find))
+                return w;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        var dic = new Dictionary();
+        dic.readDatabase();
+        var mainFrame = new MainFrame();
+        mainFrame.setDictionary(dic);
+        mainFrame.Init();
+    }
+}
---
+++ Dictionary/src/dictionary/Word.java
@@ -0,0 +1,72 @@
+package dictionary;
+
+public class Word {
+    private int id;
+    private String word;
+    private String html;
+    private String pronounce;
+    private String description;
+
+    // Constructors
+    public Word() {
+    }
+
+    public Word(int id, String word, String html, String pronounce, String description) {
+        this.id = id;
+        this.word = word;
+        this.html = html;
+        this.pronounce = pronounce;
+        this.description = description;
+    }
+    
+    public Word(String word, String html){
+        this.word = word;
+        this.html = html;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getPronounce() {
+        return pronounce;
+    }
+
+    public void setPronounce(String pronounce) {
+        this.pronounce = pronounce;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // toString() method for debugging purposes
+    @Override
+    public String toString() {
+        return word;
+    }
+}
